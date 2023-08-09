59236be00e8aa1f4237c9078c75bb94d94e57ffb
Abhishek Gupta <42316673+aadice02@users.noreply.github.com>
2023-08-10 01:38:00 +0530

    Update output.tf

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -7,15 +7,3 @@ output "websubnet_id" {
   value = azurerm_subnet.web-subnet.id
   description = "Id of websubnet in the network"
 }
-
-output "appsubnet_id" {
-  value = azurerm_subnet.app-subnet.id
-  description = "Id of appsubnet in the network"
-}
-
-output "dbsubnet_id" {
-  value = azurerm_subnet.db-subnet.id
-  description = "Id of dbsubnet in the network"
-}
-
-
